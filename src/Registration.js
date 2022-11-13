ae6908d6bb882c763c7918567ef90d48d5b377d3
azbagas <azharbagaskara1@gmail.com>
2022-11-13 18:12:35 +0700

    registration

--- src/Registration.js
+++ src/Registration.js
@@ -1,12 +1,130 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { Text, View , TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import { firebase } from '../config'
 
-export default class Registration extends Component {
-  render() {
+const Registration = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+    const [bio, setBio] = useState('')
+    const [location, setLocation] = useState('')
+
+    registerUser = async (email, password, name, age, bio, location) => {
+        await firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(() => {
+            firebase.auth().currentUser.sendEmailVerification({
+                handleCodeInApp: true,
+                url:'https://orbit-d853f.firebaseapp.com',
+            })
+            .then(() => {
+                alert('Verification email sent')
+            }).catch((error) => {
+                alert(error.message)
+            })
+            .then(() => {
+                firebase.firestore().collection('users')
+                .doc(firebase.auth().currentUser.uid)
+                .set({
+                    name,
+                    age,
+                    bio,
+                    location,
+                    email,
+                })
+            })
+            .catch((error) => {
+                alert(error.message)
+            })
+        })
+        .catch((error => {
+            alert(error.message)
+        }))
+    }
+
     return (
-      <View>
-        <Text>Registration</Text>
-      </View>
+        <View style={StyleSheet.container}>
+            <Text style={{fontWeight: 'bold', fontSize:23}}>
+                Register Here!!
+            </Text>
+            <View style={{marginTop:40}}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Name"
+                    onChangeText={(name) => setName(name)}
+                    autoCorrect={false}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Age"
+                    onChangeText={(age) => setAge(age)}
+                    autoCorrect={false}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Bio"
+                    onChangeText={(bio) => setBio(bio)}
+                    autoCorrect={false}
+                />
+                
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Location"
+                    onChangeText={(location) => setLocation(location)}
+                    autoCorrect={false}
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Email"
+                    onChangeText={(email) => setEmail(email)}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    keyboardType="email-address"
+                />
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="Password"
+                    onChangeText={(password) => setPassword(password)}
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    secureTextEntry={true}
+                />
+            </View>
+            <TouchableOpacity
+                onPress={() => registerUser(email, password, name, age, bio, location)}
+                style={styles.button}
+            >
+                <Text style={{fontWeight:'bold', fontSize:22}}>Register</Text>
+            </TouchableOpacity>
+        </View>
     )
-  }
 }
+
+export default Registration
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 0.5,
+        alignItems:'center',
+        marginTop:50,
+    },
+    textInput: {
+        paddingTop:20,
+        paddingBottom:10,
+        width: 400,
+        fontSize:20,
+        borderBottomWidth:1,
+        borderBottomColor:'#000',
+        marginBottom:10,
+        textAlign:'center',
+    },
+    button:{
+        marginTop:50,
+        height:70,
+        width:250,
+        backgroundColor:'#026efd',
+        alignItems:'center',
+        justifyContent:'center',
+        borderRadius:50,
+    }
+})
